02b14bbebac076a98ac1337b9ab31f11a1c4d307
Daniel Danopia <me@danopia.net>
2010-02-07 22:44:01 -0500

    Added some docs to control.rb; commiting to see how it looks on Yardoc's site

--- lib/luck/control.rb
+++ lib/luck/control.rb
@@ -1,7 +1,24 @@
 module Luck
+# Control is a master object that handles size methods.
+#
+# If you actually add an instance of Control to a Pane, it'll just crash :D
 class Control
   attr_accessor :pane, :display, :x1, :y1, :x2, :y2
   
+  # Create a new Control.
+  #
+  # @param [Pane] pane an instance of Luck::Pane to use for sizes
+  # @param [Number] x1 Left boundary of the control. Negative numbers
+  #   are relative to the parent's right boundary.
+  # @param [Number] y1 Top boundary of the control. Negative numbers
+  #   are relative to the parent's bottom boundary.
+  # @param [Number] x2 Right boundary of the control. Negative numbers
+  #   are relative to the parent's right boundary.
+  # @param [Number] y2 Bottom boundary of the control. Negative numbers
+  #   are relative to the parent's bottom boundary.
+  # 
+  # @yield The optional block is executed in the Control instance,
+  #   allowing for a limited DSL syntax.
   def initialize pane, x1, y1, x2, y2, &blck
     @pane = pane
     @display = pane.display
@@ -11,23 +28,35 @@ class Control
     instance_eval &blck if blck
   end
   
+  # Calculates the control's absolute left boundary on the display.
+  # @return [Number]
   def x1
     @pane.x1 + ((@x1 < 0) ? (@pane.width + @x1) : @x1)
   end
+  # Calculates the control's absolute top boundary on the display.
+  # @return [Number]
   def y1
     @pane.y1 + ((@y1 < 0) ? (@pane.height + @y1) : @y1)
   end
   
+  # Calculates the control's absolute right boundary on the display.
+  # @return [Number]
   def x2
     @pane.x1 + ((@x2 < 0) ? (@pane.width + @x2 + 1) : @x2)
   end
+  # Calculates the control's absolute bottom boundary on the display.
+  # @return [Number]
   def y2
     @pane.y1 + ((@y2 < 0) ? (@pane.height + @y2 + 1) : @y2)
   end
   
+  # Calculates the control's width.
+  # @return [Number]
   def width
     x2 - x1
   end
+  # Calculates the control's width.
+  # @return [Number]
   def height
     y2 - y1
   end
