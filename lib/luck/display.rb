071e3cabf9fbe86e78301030c3e3efaa7992801e
Daniel Danopia <me@danopia.net>
2010-02-08 00:33:03 -0500

    Doc'ed Luck::Display.

--- lib/luck/display.rb
+++ lib/luck/display.rb
@@ -1,7 +1,33 @@
 module Luck
+# Display is the main class in any Luck-using application. Display
+# manages access to the terminal and does its best to remove any
+# explicit ANSI escapes from other classes.
 class Display
-  attr_accessor :width, :height, :panes, :buffer, :dirty, :active_control
+  # @return [Number] The width of the terminal.
+  attr_reader :width
+  # @return [Number] The height of the terminal.
+  attr_reader :height
   
+  # @return [Hash<Symbol, Pane>] A Hash containing all the Panes
+  #   belonging to the display.
+  attr_reader :panes
+  
+  # Value determining which parts of the display are dirty and need to
+  # be re-drawn.
+  #
+  # @return [Boolean, Array] False means the display is clean; True
+  #   means the whole display is dirty. If the value is an Array, then
+  #   it contains a list of Pane keys that need redrawing.
+  attr_reader :dirty
+  
+  attr_accessor :active_control
+  
+  # Create a new display. If given, the block is run in the context of
+  # the new display, to allow for shorter code while creating panels
+  # and controls.
+  #
+  # NOTE: This method sets the console modes when called! To undo the
+  # modes, call Display#undo_modes
   def initialize &blck
     @panes = {}
     @dirty = true
@@ -14,23 +40,72 @@ class Display
     instance_eval &blck if blck
   end
   
+  # Creates a new pane and adds it to the display. If given, the block
+  # is run in the context of the new Pane to allow for shorter code
+  # while creating controls.
+  #
+  # @param name The key that will be used to refer to the pane. I
+  #   recommend using Symbols.
+  # @param args The arguments to pass to Pane.new
+  #
+  # @see Pane#initialize
   def pane name, *args, &blck
     @panes[name] = Pane.new(self, *args, &blck)
   end
   
+  # Place a string at some coordinates on the display. Used mostly by
+  # panes and controls to render.
+  #
+  # NOTE: This method does not flush stdout, so the text may not show up
+  # until you flush the display! (Rendering method don't need to; the
+  # Display flushes after it's done rendering controls)
+  #
+  # @param [Number] row The y-coordinate, from the top.
+  # @param [Number] col The x-coordinate, from the left.
+  # @param [String] text The text to print.
+  #
+  # @example Say Hello at (10, 5)
+  #   display.place(5, 10, "Hello")
   def place row, col, text
     print "\e[#{row.to_i};#{col.to_i}H#{text}"
   end
+  
+  # Encases ANSI color codes in a command sequence. This is useful for
+  # embeding in strings.
+  #
+  # NOTE: This does not print anything to the screen!
+  #
+  # @todo Abstract out color codes to make it less ANSI-specific, i.e.
+  #   color(:red)
+  #
+  # @param [String, Number] codes The ANSI color codes to encapsulate.
+  # @return [String] An ANSI escape sequence.
+  #
+  # @example Print "Hello" in red, and then reset back
+  #   puts "#{color 34}Hello!#{color 0}"
   def color codes
     "\e[#{codes}m"
   end
   
+  # Shows or hides the cursor.
+  #
+  # NOTE: This method flushes stdout, so you don't need to handle that
+  # yourself.
+  #
+  # @param [Boolean] show True to show the cursor, False to hide it.
   def cursor=(show)
-    print "\e[?25h" if show
-    print "\e[?25l" unless show
+    print show ? "\e[?25h" : "\e[?25l"
     $stdout.flush
   end
   
+  # Mark part or all of the display as dirty. Dirty portions will be
+  # redrawn next loop.
+  #
+  # @param pane The key of a pane to mark as dirty. If nil, the entire
+  #   display will be marked as dirty.
+  #
+  # @example Redraw the :main pane next loop
+  #   display.dirty! :main
   def dirty! pane=nil
     if pane && @dirty.is_a?(Array) && !(@dirty.include?(pane))
       @dirty << pane
@@ -41,6 +116,8 @@ class Display
     end
   end
   
+  # Handles the display. This handles redrawing dirty panels, reading
+  # input, and resizing the display if the terminal has changed sizes.
   def handle
     handle_stdin
     
@@ -56,6 +133,10 @@ class Display
     @dirty = false
   end
   
+  # Force an immediate redraw of the entire display or certain panes.
+  #
+  # @param [Array, True] panes An array of pane keys to redraw. If not
+  #   specified (or True), then the entire display is redrawn.
   def redraw panes=true
     print "\e[H\e[J" if panes == true # clear all and go home
     self.cursor = active_control
@@ -70,6 +151,7 @@ class Display
     $stdout.flush
   end
   
+  # Handle standard input.
   def handle_stdin
     $stdin.read_nonblock(1024).each_char do |chr|
       active_control.handle_char chr if active_control
@@ -86,14 +168,17 @@ class Display
   # this is all copied from cashreg :P #
   ######################################
 
-  # yay grep
   TIOCGWINSZ = 0x5413
   TCGETS = 0x5401
   TCSETS = 0x5402
   ECHO   = 8 # 0000010
   ICANON = 2 # 0000002
 
-  # thanks google for all of this
+  # Gets the terminal size.
+  #
+  # Thanks google for all of this.
+  #
+  # @return [Array<Number>] [rows, columns]
   def terminal_size
     rows, cols = 25, 80
     buf = [0, 0, 0, 0].pack("SSSS")
@@ -103,7 +188,9 @@ class Display
     return [rows, cols]
   end
 
-  # had to convert these from C... fun
+  # Prepares the terminal for fun TUI things. Turns off echo and has the
+  # terminal give us each char as it is pressed without waiting for the
+  # user to press enter.
   def prepare_modes
     buf = [0, 0, 0, 0, 0, 0, ''].pack("IIIICCA*")
     $stdout.ioctl(TCGETS, buf)
@@ -114,7 +201,12 @@ class Display
     $stdout.ioctl(TCSETS, new_modes.pack("IIIICCA*"))
     self.cursor = false
   end
-  def undo_modes # restore previous terminal mode
+  
+  # Restores previous terminal modes. Use to reset the changes that is
+  # done by prepare_modes.
+  #
+  # @see Display#prepare_modes
+  def undo_modes
     $stdout.ioctl(TCSETS, @old_modes.pack("IIIICCA*"))
     print "\e[2J\e[H" # clear all and go home
     self.cursor = true # show the mouse
